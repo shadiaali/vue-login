ea140d75c4111c19fca48d0a427f2bf11a7e72c3
Ali <s_ali59@tss-hr420-dm11.fc.ca>
2019-01-31 17:11:45 -0500

    clientside login

--- includes/functions.php
+++ includes/functions.php
@@ -2,6 +2,25 @@
 
 include 'connect.php';
 
+function validate_login($pdo, $user, $password) {
+  $query = "SELECT * FROM users WHERE first_name = '$user'";
+
+  $get_user = $pdo->query($query);
+  $results = array();
+
+  while($row =$get_user->fetch(PDO::FETCH_ASSOC)) {
+    $results[] = $row;
+    if ($row['password'] == $password){
+      $row['password'] = 'XXXXXXXXXX';
+      $results[] = $row;
+    }else{
+    $results[] = "false";
+}
+}
+    return $results;
+}
+
+
 function get_single_user($pdo, $user) {
   $query = "SELECT * FROM users WHERE id = '$user'";
 
--- includes/index.php
+++ includes/index.php
@@ -4,7 +4,13 @@
 
   //remember to use the techniques pan is showing you to clean up
   //user input (prepared statements, etc)
+// do authentication (password validation) first
 
+if (isset($_GET['username'])) {
+  $data = validate_login($conn, $_GET['username'], $_GET['password']);
+  echo json_encode($data);
+
+}
   if(isset($_GET['user'])) {
     $data = get_single_user($conn, $_GET['user']);
     echo json_encode($data);
